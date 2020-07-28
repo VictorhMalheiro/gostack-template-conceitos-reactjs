95b0540b5e344e6ac32a3dce42ac124bae69c492
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2020-07-28 15:00:09 -0300

    Revert "Desafio Finalizado ✅"
    This reverts commit d77d221beebc0e7aec6de6403f2ed4ace79d4a53.

--- src/App.js
+++ src/App.js
@@ -1,85 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import api from './services/api';
 import "./styles.css";
 
 function App() {
-  const [repositories, setRepositories] = useState([]);
-  const [newTech, setNewTech] = useState('');
-
-  useEffect(() => {
-    api.get('repositories').then(response => {
-        setRepositories(response.data);
-    });
-  }, [repositories]);
-
   async function handleAddRepository() {
-    if (newTech) {
-      const response = await api.post('repositories', {
-        url: "new-list",
-        title: newTech,
-        techs: [
-          "React Native",
-          "React",
-          "Node",
-          "Adonis"
-        ]
-      });
-  
-      const repository = response.data;
-  
-      setRepositories([...repositories, repository]);
-      setNewTech('');
-    } else {
-      alert('Adicione um titulo!');
-    }
+    // TODO
   }
-
-
-  async function handleAddLike(id) {
-    await api.post(`repositories/${id}/like`);
-  }
-
 
   async function handleRemoveRepository(id) {
-    const repositoriesListCopy = repositories.filter(repository => repository.id !== id);
-    
-    await api.delete(`repositories/${id}`);
-    
-    setRepositories(repositoriesListCopy);
+    // TODO
   }
 
   return (
     <div>
-        <div className="rowInput">
-          <input
-            placeholder="Nome do repositório"
-            className="addRepository"
-            type="text"
-            onChange={text => setNewTech(text.target.value)}
-            value={newTech}
-          />
-          <button type="submit" onClick={handleAddRepository}>Adicionar</button>
-        </div>
-
       <ul data-testid="repository-list">
-        {repositories.map(repository => (
-          <div className="repository" key={repository.id}>
-            <li className="title">
-                <span>{repository.title}</span>
-                <button onClick={() => handleRemoveRepository(repository.id)}>
-                  Remover
-                </button>
-            </li>
-            <div className="techs">
-              <button onClick={() => handleAddLike(repository.id)} className="likes">Likes {repository.likes}</button>
-              <span>{repository.techs[1]}</span>
-              <span>{repository.techs[2]}</span>
-              <span>{repository.techs[3]}</span>
-            </div>
-        </div>
-        ))}
+        <li>
+          Repositório 1
+
+          <button onClick={() => handleRemoveRepository(1)}>
+            Remover
+          </button>
+        </li>
       </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
